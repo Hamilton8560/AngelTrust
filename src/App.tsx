48d566330a664b6d6d7b4321f2eeb055eaed2913
David Hamilton <davidhamilton@macbookpro.mynetworksettings.com>
2024-12-28 12:16:11 -0500

    routing fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { BlogPage } from "./pages/blog";
 import { BlogPostPage } from "./pages/blog/BlogPostPage";
 import { CategoryPage } from "./pages/blog/CategoryPage";
 import { ContactPage } from "./pages/contact";
-import { SearchProvider } from "./contexts/SearchContext";
 import { PrivacyPolicy } from "./pages/legal/PrivacyPolicy";
 import { Disclaimer } from "./pages/legal/Disclaimer";
 import { TermsOfService } from "./pages/legal/TermsOfService";
@@ -18,33 +17,31 @@ export default function App() {
   const basename = import.meta.env.BASE_URL || "/";
 
   return (
-    <SearchProvider>
-      <Router basename={basename}>
-        <Layout>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <div id="hero">
-                    <Hero />
-                  </div>
-                  <ServicesPage />
-                  <AboutPage />
-                  <BlogPage />
-                  <ContactPage />
-                </>
-              }
-            />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/blog/category/:category" element={<CategoryPage />} />
-            <Route path="/blog/:slug" element={<BlogPostPage />} />
-            <Route path="/privacy" element={<PrivacyPolicy />} />
-            <Route path="/terms" element={<TermsOfService />} />
-            <Route path="/disclaimer" element={<Disclaimer />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </SearchProvider>
+    <Router basename={basename}>
+      <Layout>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div id="hero">
+                  <Hero />
+                </div>
+                <ServicesPage />
+                <AboutPage />
+                <BlogPage />
+                <ContactPage />
+              </>
+            }
+          />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/category/:category" element={<CategoryPage />} />
+          <Route path="/blog/:slug" element={<BlogPostPage />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          <Route path="/disclaimer" element={<Disclaimer />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }

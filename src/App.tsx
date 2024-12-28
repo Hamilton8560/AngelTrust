49ffb72dd623f904af949c445bd8f32d22623d55
David Hamilton <davidhamilton@macbookpro.mynetworksettings.com>
2024-12-28 12:03:32 -0500

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,43 @@ import { BlogPage } from "./pages/blog";
 import { BlogPostPage } from "./pages/blog/BlogPostPage";
 import { CategoryPage } from "./pages/blog/CategoryPage";
 import { ContactPage } from "./pages/contact";
+import { SearchProvider } from "./contexts/SearchContext";
+import { PrivacyPolicy } from "./pages/legal/PrivacyPolicy";
+import { TermsOfService } from "./pages/legal/TermsOfService";
+import { Disclaimer } from "./pages/legal/Disclaimer";
 
 export default function App() {
   // Use Vite's import.meta.env instead of process.env
   const basename = import.meta.env.BASE_URL || "/";
 
   return (
-    <Router basename={basename}>
-      <Layout>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <div id="hero">
-                  <Hero />
-                </div>
-                <ServicesPage />
-                <AboutPage />
-                <BlogPage />
-                <ContactPage />
-              </>
-            }
-          />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/category/:category" element={<CategoryPage />} />
-          <Route path="/blog/:slug" element={<BlogPostPage />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <SearchProvider>
+      <Router basename={basename}>
+        <Layout>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <div id="hero">
+                    <Hero />
+                  </div>
+                  <ServicesPage />
+                  <AboutPage />
+                  <BlogPage />
+                  <ContactPage />
+                </>
+              }
+            />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blog/category/:category" element={<CategoryPage />} />
+            <Route path="/blog/:slug" element={<BlogPostPage />} />
+            <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Route path="/terms" element={<TermsOfService />} />
+            <Route path="/disclaimer" element={<Disclaimer />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </SearchProvider>
   );
 }
